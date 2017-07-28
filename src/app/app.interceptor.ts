df671d48210c2a9ebb6c3567da7f8a6c2c2f47a7
kelvin <kelvin@admir.tv>
2017-07-28 17:14:15 +0100

    token out

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -8,7 +8,7 @@ export class AppInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         const authReq = req.clone({
-            headers: req.headers.set('Authorization', 'token 07c7a1b3193fc0f1c63cdd34b38682657c7a0083')
+            headers: req.headers.set('Authorization', 'token ...')
         });
 
         return next.handle(authReq);
